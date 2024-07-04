5e67cd6a360b22f9d80ff22e7fe3ff280eeddc9b
Ikponmwenosa Ediagbonya <ediagbonya213@gmail.com>
2024-07-04 10:28:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTimeAndDay();
     setInterval(updateTimeAndDay, 1000);
 });
+
+let menuList = document.querySelector('#menu-list');
+let menuBar = document.querySelector('.menu-bar');
+
+menuList.style.maxHeight = "0px";
+
+menuBar.addEventListener('click', (e)=>{
+
+    if (menuList.style.maxHeight == '0px'){
+
+     menuList.style.maxHeight = '400px';
+
+    }else {
+
+        menuList.style.maxHeight = '0px';
+
+    }
+});
+
+
